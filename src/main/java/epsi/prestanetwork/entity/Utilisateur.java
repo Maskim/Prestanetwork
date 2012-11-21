b76d001254207c463e558314706da898cbc9d0e0
Meidi Airouche <meidi.airouche@gmail.com>
2012-11-21 18:37:45 +0100

    Inscription user : 1er test

---
+++ src/main/java/epsi/prestanetwork/entity/Utilisateur.java
@@ -0,0 +1,22 @@
+package epsi.prestanetwork.entity;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Meidi
+ */
+public class Utilisateur {
+    
+    private String prenom;
+    
+    public String getPrenom()
+    {
+        return "Meidi";
+    }
+
+    
+}
